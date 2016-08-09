ff481bd4677d1dd7b32f406b483f96b232798a06
Tom McKenzie <tom@chillidonut.com>
2016-08-09 13:03:49 +1000

    Adds tests, Object.assign polyfill

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v1.0.0-rc.2
+
+Updates tests, includes objectAssign polyfill
+
 ## v1.0.0-rc.1
 
 Release candidate, forked from <https://github.com/JedWatson/classnames>'s bind.js
--- index.js
+++ index.js
@@ -5,31 +5,10 @@
 */
 /* global define */
 
-/*
-
-Usage notes:
-
-```
-const markupStyles = { container: { backgroundColor: 'white', margin: '10px' } },
-	otherStyles = { container: { backgroundColor: 'red', color: 'white' }, outer: { margin: 10 } }
-
-import classStyles from 'class-styles'
-const cx = classStyles.bind([ markupStyles, otherStyles ])
-
-cx('container outer')
-// => { margin: '10px', backgroundColor: 'red', color: 'white' }
-cx({ padding: 10 }, 'not-exist')
-// => { padding: 10 }
-```
-
-All markup.container then other.container styles should be applied, then
-markup.second and other.second.
-
-*/
-
 (function () {
 	'use strict';
 
+	var objectAssign = require('object-assign')
 	var hasOwn = {}.hasOwnProperty;
 
 	function classStyles () {
@@ -58,9 +37,9 @@ markup.second and other.second.
 
 		return styles.reduce(
 			(result, style) => (typeof style === 'object'
-				? Object.assign(result, style)
-				: Object.assign(result,
-						this.reduce((keyResult, boundStyle) => Object.assign(keyResult, boundStyle[style]), {})
+				? objectAssign(result, style)
+				: objectAssign(result,
+						this.reduce((keyResult, boundStyle) => objectAssign(keyResult, boundStyle[style]), {})
 					)
 			), {})
 	}
